b4258f2c2122080dc6faafa3cead08a84ca99ae7
omarfrontendev <omar.abdelrhman.dev@gmail.com>
2022-11-03 10:37:04 +0200

    updates

--- src/Page/OTTAudio/index.jsx
+++ src/Page/OTTAudio/index.jsx
@@ -13,6 +13,7 @@ const initialize = {
   primary__video__genre: "Alternatives",
   explicit__image__content: 'No Explicit',
   explicit__video__content: 'No Explicit',
+  stores: []
 }
 
 const OTTAudio = () => {
@@ -21,6 +22,7 @@ const OTTAudio = () => {
   const [audio, setAudio] = useState(false);
   const [image, setImage] = useState(false);
   const [video, setVideo] = useState(false);
+  const [selectAll, setSelectAll] = useState(false);
 
   const audioData = [
     {
@@ -286,6 +288,184 @@ const OTTAudio = () => {
     }
   ];
 
+  const stories = [
+    {
+      value: 'rn_direct',
+      text: 'RN Direct',
+      img: 'https://www.routenote.com/rn/img/icon7.png'
+    },
+    {
+      value: 'iTunes__&__Apple Music',
+      text: 'iTunes & Apple Music',
+      img: 'https://www.routenote.com/rn/img/icon1.png'
+    },
+    {
+      value: 'amazon',
+      text: 'Amazon',
+      img: 'https://www.routenote.com/rn/img/Amazon_Store_Icon.png'
+    },
+    {
+      value: 'eMUSIC',
+      text: 'eMUSIC',
+      img: 'https://www.routenote.com/rn/img/icon3.png'
+    },
+    {
+      value: 'Spotify',
+      text: 'Spotify',
+      img: 'https://www.routenote.com/rn/img/icon4.png'
+    },
+    {
+      value: 'youTube__music',
+      text: 'YouTube Music',
+      img: 'https://www.routenote.com/rn/img/youtube-music-manage-stores-icon.png'
+    },
+    {
+      value: 'anghami',
+      text: 'Anghami',
+      img: 'https://www.routenote.com/rn/img/anghami.png'
+    },
+    {
+      value: 'deezer',
+      text: 'Deezer',
+      img: 'https://www.routenote.com/rn/img/icon16.png'
+    },
+    {
+      value: 'WiMp/Tidal',
+      text: 'WiMp/Tidal',
+      img: 'https://www.routenote.com/rn/img/wimp.png'
+    },
+    {
+      value: 'Claro-musica',
+      text: 'Claro-musica',
+      img: 'https://www.routenote.com/rn/img/claro.png'
+    },
+    {
+      value: 'Nuuday',
+      text: 'Nuuday',
+      img: 'https://www.routenote.com/rn/img/nuuday_manage_store.png'
+    },
+    {
+      value: 'SoundCloud',
+      text: 'SoundCloud',
+      img: 'https://www.routenote.com/rn/img/soundcloud-logo_360.png'
+    },
+    {
+      value: 'Gracenote',
+      text: 'Gracenote',
+      img: 'https://www.routenote.com/rn/img/Gracenote_logo.png'
+    },
+    {
+      value: 'Kanjian',
+      text: 'Kanjian',
+      img: 'https://www.routenote.com/rn/img/kanjianlogo.png'
+    },
+    {
+      value: 'Pandora',
+      text: 'Pandora',
+      img: 'https://www.routenote.com/rn/img/pandora-icon.png'
+    },
+    {
+      value: 'Napster',
+      text: 'Napster',
+      img: 'https://www.routenote.com/rn/img/Napster_Manage_Stores.png'
+    },
+    {
+      value: 'Melon',
+      text: 'Melon',
+      img: 'https://www.routenote.com/rn/img/Melonicon.jpg'
+    },
+    {
+      value: 'iHeartRadio',
+      text: 'iHeartRadio',
+      img: 'https://www.routenote.com/rn/img/rn_iHeartRadio.png'
+    },
+    {
+      value: '7Digital',
+      text: '7Digital',
+      img: 'https://www.routenote.com/rn/img/7Digital.png'
+    },
+    {
+      value: 'Saavn',
+      text: 'Saavn',
+      img: 'https://www.routenote.com/rn/img/Jiosaavn_ManageStores.jpg'
+    },
+    {
+      value: 'Bugs!',
+      text: 'Bugs!',
+      img: 'https://www.routenote.com/rn/img/Bugs_ManageStores.png'
+    },
+    {
+      value: 'Facebook',
+      text: 'Facebook',
+      img: 'https://www.routenote.com/rn/img/FaceInsta_ManageStores.png'
+    },
+    {
+      value: 'VIBE',
+      text: 'VIBE',
+      img: 'https://www.routenote.com/rn/img/vibe-manage-stores.png'
+    },
+    {
+      value: 'Netease',
+      text: 'Netease',
+      img: 'https://www.routenote.com/rn/img/NetEaseManageStores.png'
+    },
+    {
+      value: 'Tencent',
+      text: 'Tencent',
+      img: 'https://www.routenote.com/rn/img/Tencent_ManageStores.jpg'
+    },
+    {
+      value: 'AWA',
+      text: 'AWA',
+      img: 'https://www.routenote.com/rn/img/AWA_ManageStores.jpg'
+    },
+    {
+      value: 'TikTok',
+      text: 'TikTok',
+      img: 'https://www.routenote.com/rn/img/tiktok-manage-logo.png'
+    },
+    {
+      value: 'Kuack Media',
+      text: 'Kuack Media',
+      img: 'https://www.routenote.com/rn/img/Bugs_ManageStores.png'
+    },
+    {
+      value: 'Qobuz',
+      text: 'Qobuz',
+      img: 'https://www.routenote.com/rn/img/Qobuz_ManageStores.jpg'
+    },
+    {
+      value: 'FLO',
+      text: 'FLO',
+      img: 'https://www.routenote.com/rn/img/Flo-manage.png'
+    },
+    {
+      value: 'KKBOX',
+      text: 'KKBOX',
+      img: 'https://www.routenote.com/rn/img/kkbox-manage-stores.png'
+    },
+    {
+      value: 'Zing MP3',
+      text: 'Zing MP3',
+      img: 'https://www.routenote.com/rn/img/zing-manage-stores.png'
+    },
+    {
+      value: 'Joox',
+      text: 'Joox',
+      img: 'https://www.routenote.com/rn/img/joox-manage-stores.png'
+    },
+    {
+      value: 'Hungama',
+      text: 'Hungama',
+      img: 'https://www.routenote.com/rn/img/hungama-manage-stores.png'
+    },
+    {
+      value: 'YouTube',
+      text: 'YouTube',
+      img: 'https://www.routenote.com/rn/img/youtube-icon.png'
+    },
+  ];
+
   const addFileHandler = (input) => {
     let files = input.target.files;
     let reader = new FileReader();
@@ -303,6 +483,31 @@ const OTTAudio = () => {
       setData({...data, [input.target.id]: e.target.result})
     };
     reader.readAsDataURL(files[0]);
+  };
+
+  const selectBox = e => {
+    setSelectAll(false)
+    if(e.target.checked) {
+      setData({...data, stores: [...data.stores, e.target.value]})
+    }else {
+      const arr = data.stores.filter(i => i !== e.target.value)
+      setData({...data, stores: arr})
+    }
+  };
+
+  const selectAllHanderl = e => {
+    const arr = [];
+    if(e.target.checked) {
+      setData({...data, stores: []})
+      stories.map(story => {
+        arr.push(story.value)
+      })
+      setSelectAll(true)
+    }else {
+      setSelectAll(false)
+      setData({...data, stores: []})
+    }
+    setData({...data, stores: arr})
   }
 
 
@@ -316,8 +521,8 @@ const OTTAudio = () => {
     >
       {audioData.map(box => (
         <div className='main__box' key={box.id}>
-          <h2 className='box__title'>{box.title}</h2>
           <Card>
+            <h2 className='box__title'>{box.title}</h2>
             <div className='input__group'>
               {box.resources.map(input => (
                 input.type === 'select__box' ? (
@@ -344,7 +549,7 @@ const OTTAudio = () => {
                         required
                         onChange={addFileHandler}
                         />
-                    </div >
+                    </div>
                     {input.id === 'track__file' && <div className='file__result'>
                       {audio ? <audio controls src={audio} /> : <p>No File!</p>}
                     </div>}
@@ -373,7 +578,38 @@ const OTTAudio = () => {
           </Card>
         </div>
       ))}
-      <button className='submit__btn' type='submit'>Submit</button>
+      <Card>
+        <h2 className='checkbox__title'>Choose Your Stores
+          <div>
+            <label htmlFor='select__all'>Select All</label>
+            <input 
+              id='select__all'
+              type='checkbox'
+              onChange={selectAllHanderl}
+              value='select__all'
+              checked={selectAll}
+            />
+          </div>
+        </h2>
+        <div className='checkbox__inputs__group'>
+          {stories.map(input => (
+            <div className='checkbox' key={input.value}>
+              <label htmlFor={input.value}>
+                <img src={input.img} alt={'platform__image'} />
+                {input.text}
+              </label>
+              <input 
+                id={input.value}
+                type='checkbox'
+                value={input.value}
+                onChange={selectBox}
+                checked={data.stores.includes(input.value)}
+              />
+            </div>
+          ))}
+        </div>
+      </Card>
+      <button className='submit__btn' style={{marginTop: '40px'}} type='submit'>Submit</button>
     </form>
   )
 }
--- src/Page/OTTImage/index.jsx
+++ src/Page/OTTImage/index.jsx
@@ -1,10 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { BsFillCloudUploadFill } from 'react-icons/bs'
+import Card from '../../Components/Card'
 
 import './OTTImage.css'
 
 const OTTImage = () => {
+
+  const [data, setData] = useState({
+    image__title: "",
+    images: []
+  });
+
+  const addFileHandler = (input) => {
+    let files = input.target.files;
+    let reader = new FileReader();
+
+    reader.onload = e => {
+      setData({...data, images: [...data.images, e.target.result]})
+    };
+    reader.readAsDataURL(files[0]);
+  }
+
   return (
-    <div>OTTAImage</div>
+    <div className='ott__image__page'>
+      <div className="images">
+        {data.images.map((img, i) => (
+          <img key={i} src={img} alt="" />
+        ))}
+      </div>
+      <form 
+        className='main__form' 
+        onSubmit={e => {
+          e.preventDefault()
+        }}
+      >
+        <Card>
+          <div className='input__group'>
+            <div className='input__controle'>
+              <label htmlFor='image__title'>Image Title:</label>
+              <input 
+                id='image__title'
+                type='text'
+                onChange={(e) => setData({...data, image__title: e.target.value})}
+                value={data.image__title || ''}
+                required
+              />
+            </div>
+            <div className='input__controle input__file__controle'>
+              <label className='add__button' htmlFor='image__file'><BsFillCloudUploadFill />Upload image</label>
+              <input 
+                id='image__file'
+                type='file'
+                required
+                onChange={addFileHandler}
+                style={{display: 'none'}}
+              />
+            </div>
+          </div>
+        </Card>
+        <button className='submit__btn' type='submit'>Submit</button>
+      </form>
+    </div>
   )
 }
 
